fbe93a0bf1c11b1b4d32cf355866107b7c71eff1
send2ugfd <send2ugfd@welgram.com>
2023-08-08 17:55:53 +0900

    Entity 추가해야함(위치 확인 필요)

--- src/main/java/com/example/studyBoard/controller/loginController.java
+++ src/main/java/com/example/studyBoard/controller/loginController.java
@@ -1,14 +1,16 @@
 package com.example.studyBoard.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class loginController {
 
-    @GetMapping("/login")
+    @GetMapping("/login.html")
     @ResponseBody
     public String login(Model model) throws Exception{
-        return "login.html";
+        return "test";
     }
 }
---
+++ src/main/java/com/example/studyBoard/entity/login.java
@@ -0,0 +1,13 @@
+package com.example.studyBoard.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@Getter
+@Setter
+@EntityScan
+public class login {
+}
